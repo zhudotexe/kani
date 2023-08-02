44c119f44b878ff72c839ff5a137ef5e306928c6
Andrew Zhu <me@andrew-zhu.com>
2023-08-02 16:03:42 -0700

    docs: move 4b quant to engines

--- examples/4_engines_fp4_quant.py
+++ examples/4_engines_fp4_quant.py
@@ -21,7 +21,7 @@ engine = LlamaEngine(
 ai = Kani(
     engine,
     system_prompt=(
-        "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. "
+        "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe."
         " Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content."
         " Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not"
         " make any sense, or is not factually coherent, explain why instead of answering something not correct. If you"
