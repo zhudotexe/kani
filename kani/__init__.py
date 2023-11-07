efbdfe5b00509ce39c768113c2c09a5c5dd6ad5b
Andrew Zhu <me@andrew-zhu.com>
2023-11-07 17:06:00 -0500

    chore: kani.ToolCall

--- kani/__init__.py
+++ kani/__init__.py
@@ -2,7 +2,7 @@ from . import engines, exceptions, utils
 from .ai_function import AIFunction, AIParam, ai_function
 from .internal import ExceptionHandleResult, FunctionCallResult
 from .kani import Kani
-from .models import ChatMessage, ChatRole, FunctionCall, MessagePart
+from .models import ChatMessage, ChatRole, FunctionCall, MessagePart, ToolCall
 from .utils.cli import chat_in_terminal, chat_in_terminal_async
 
 # declare that kani is also a namespace package
