a8fef2b5835a91c5b830c40078f309e05c0d7822
Andrew Zhu <me@andrew-zhu.com>
2024-04-08 13:31:53 -0400

    chore: isort

--- examples/mistral_gguf.py
+++ examples/mistral_gguf.py
@@ -3,7 +3,6 @@ A standalone Mistral-7B-Instruct file using llama.cpp.
 """
 
 from kani import Kani, chat_in_terminal
-
 from kani.engines.llamacpp import LlamaCppEngine
 
 engine = LlamaCppEngine(repo_id="TheBloke/Mistral-7B-Instruct-v0.2-GGUF", filename="*.Q4_K_M.gguf")
