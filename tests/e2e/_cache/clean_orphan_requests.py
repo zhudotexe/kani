4a175ee311764836b9bd18af7835d7b995dcda45
Andrew Zhu <me@andrew-zhu.com>
2025-10-20 17:14:27 -0400

    docs: remove old llamacpp example

---
+++ tests/e2e/_cache/clean_orphan_requests.py
@@ -0,0 +1,21 @@
+"""
+Clean up any _request.http and <hash>.prompt.txt without a corresponding response.
+"""
+
+from pathlib import Path
+
+cache_root = Path(__file__).parent
+
+# api
+for fp in cache_root.glob("*/*/_request.http"):
+    if len(list(fp.parent.iterdir())) == 1:
+        fp.unlink()
+        fp.parent.rmdir()
+        print(fp.parent)
+
+# local
+for fp in cache_root.glob("*/*/prompt.txt"):
+    if len(list(fp.parent.iterdir())) == 1:
+        fp.unlink()
+        fp.parent.rmdir()
+        print(fp.parent)
