5fddbea14c57a7314c7177aa9e68304db3623029
Andrew Zhu <me@andrew-zhu.com>
2025-10-23 16:40:29 -0400

    fix: don't explode if parent model card does not exist

--- kani/utils/huggingface.py
+++ kani/utils/huggingface.py
@@ -1,14 +1,21 @@
 """Various internal utilities to interface with the HF API."""
 
 import functools
+import logging
 
 from huggingface_hub import RepoCard
+
+log = logging.getLogger(__name__)
 
 
 @functools.cache
 def get_base_models(model_id: str) -> list[str]:
     """Get the base model(s) of a model by reading its model card."""
-    model_card = RepoCard.load(model_id)
+    try:
+        model_card = RepoCard.load(model_id)
+    except Exception as e:
+        log.warning("Could not load model card for listed parent model:", exc_info=e)
+        return []
     base = getattr(model_card.data, "base_model", None)
     if not base:
         return []
